6061bd2f1fe707bc5ebd479a75b951087984f48f
geert.van.bastelaere <geert.van.bastelaere@036636dd-6947-0410-a7b4-15e2358c35dd>
2010-04-04 18:30:00 +0000

    Authentication

---
+++ trackfreaks/php_src/services/Organization.php
@@ -0,0 +1,20 @@
+<?php
+
+class Organization {
+	
+	/**
+	 * @var int
+	 */
+	var $organization_id;
+	
+	/**
+	 * @var string
+	 */
+	var $name;
+	
+	/**
+	 * @var string
+	 */
+	var $url;
+		
+}
---
+++ trackfreaks/php_src/services/Trackday.php
@@ -0,0 +1,25 @@
+<?php
+
+class Trackday {
+	
+	/**
+	 * @var int
+	 */
+	var $trackday_id;
+	
+	/**
+	 * @var int
+	 */
+	var $track_id;
+	
+	/**
+	 * @var int
+	 */
+	var $organization_id;
+	
+	/**
+	 * @var string
+	 */
+	var $date;
+	
+}
